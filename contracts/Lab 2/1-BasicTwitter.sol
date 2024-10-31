ab317603171799a667b313788e9b9d18e74f2ba6
R-Ohm <ittiphat.k@gmail.con>
2024-10-31 14:55:23 +0700

    continue on lab 2

--- contracts/Lab 2/1-BasicTwitter.sol
+++ contracts/Lab 2/1-BasicTwitter.sol
@@ -9,22 +9,42 @@ pragma solidity ^0.8.26;
 // 5️⃣ Add array of tweets 
 
 contract Twitter {
-    // Key -> Value
-    //Username => List of tweets
-    mapping(address => string[]) public tweets;
 
-    function createTweet(string memory _tweet) public {
-        //Focus => "Hello, World"
-        //Focus => ["Hello, World". "Have a nice day...", ...
-        tweets[msg.sender].push(_tweet);
+    struct Tweet {
+        string content;
+        address user_address;
+        uint16 likes;
+        uint256 timestamp;
     }
 
-    function getTweet(address _addr, uint256 _i) public view returns (string memory) {
+    // Key -> Value
+    // Username => List of tweets
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        // Ittiphat => ["Hello, World". "Have a nice day...", ...]
+        //
+        // Java:
+        // Tweet t1 = new Tweet();
+
+        // byte(string_var).length => The length of the string.
+        require(bytes(_tweet).length <= 255, "The length of your tweet exceeds the limit bro!");
+
+        Tweet memory newTweet = Tweet({
+            content: _tweet,
+            user_address: msg.sender,
+            likes: 0,
+            timestamp: block.timestamp
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _addr, uint256 _i) public view returns (Tweet memory) {
         return tweets[_addr][_i];
     }
 
-    function getAllTweets(address _addr) public view returns (string[] memory){
+    function getAllTweets(address _addr) public view returns (Tweet[] memory){
         return tweets[_addr];
-
     }
- }
+}
